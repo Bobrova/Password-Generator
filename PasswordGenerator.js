cf7a71c8df74f285181d57b1d07616761f89797b
Nastya Bobrova <deepkilt89@gmail.com>
2018-10-12 20:33:25 +0300

    PG

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -49,7 +49,7 @@
     }
 
     function validateLength(length){
-        if ( length > 3 || length < 50){
+        if ( length > 3 && length < 50){
           return length;
         }
         else return 6;
